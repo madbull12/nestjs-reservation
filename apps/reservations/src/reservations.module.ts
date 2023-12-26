f877e4cc9b50ad10cda4482109d540d9a6a691e9
madbull12 <huangandrian02@gmail.com>
2023-12-26 18:17:10 +0700

    class validator

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,22 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
-import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     DatabaseModule,
     DatabaseModule.forFeature([{ name: ReservationDocument.name,schema:ReservationSchema }]),
-    LoggerModule.forRoot({
-      pinoHttp:{
-        transport:{
-          target:'pino-pretty'
-        }
-      }
-    })
+    LoggerModule
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
